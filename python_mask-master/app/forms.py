ce197c55e8e7469ac82b08c16cd1eeb3dccabd61
WiseLook <1656438881@qq.com>
2025-12-03 10:38:15 +0800

    style: change file dir.

--- python_mask-master/app/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from app.models import Product
-
-
-class CommitForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        fields = ['title', 'type', 'contact', 'location', 'phone', 'weixin', 'expire']
